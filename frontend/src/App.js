f4b57499b5dbcfe5c0c7222fe0db742c817c5288
unknown <antea.solic@estudent.hr>
2023-03-18 18:41:53 +0100

    feat: add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-import Home from "./pages/Home/Home";
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from "./pages/LogIn/Login";
-import Signup from "./pages/SignUp/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-  
- return(
-  <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/signup" element={<Signup />} />
-</Routes>
- )
+  return (
+    <Dashboard />
+  );
 }
 
 export default App;
--- frontend/src/components/DashboardSections/DashboardSections.js
+++ frontend/src/components/DashboardSections/DashboardSections.js
@@ -1,36 +1,22 @@
 import React from 'react';
+import logo from '../../assets/images/ikona_spider.png';
 
-import Sections from './components/Sections/Sections.js';
-
-import logo from '../../assets/images/spiderweb.png';
 
 import './DashboardSections.scss';
 
 
 function DashboardSections() {
-    const listItems = [
-        'Threat Intelligence',
-        'Real-time Monitoring',
-        'Alerts',
-        'Reporting and Analysis',
-        'System parameters',
-      ];
+    
     return (
         
-        <div className='DashboardSections h-full '>
-           <h1 className= ''> <img src={logo} class="mx-auto py-4 w-1 lg:w-1/2 p-4"  ></img></h1>
-            <h2 className=' companyName font-bold text-6xl text-center' >Spider Cyber Security</h2>
-            <ul className='lista  rounded-md shadow-md p-6  '>
-            {listItems.map((item, index) => (
-                <li key={index} className="py-4 text-3xl">
-           
-            {item}
-          </li>
-        ))}
-            </ul>
-             
-            <Sections />
-        </div>
+        <header className=" bg-blue-900  flex items-center justify-between px-4 py-3 border-b-2 border-color: rgb(71,86,180) rounded-b-lg">
+  <div className="flex items-center ">
+    <img src={logo} alt="ikona logo" className="h-8 w-8 mr-2" />
+    <span className="text-2xl font-bold">SPS</span>
+  </div>
+  <span className="text-sm font-medium">username</span>
+</header>
+       
     )
 }
 
