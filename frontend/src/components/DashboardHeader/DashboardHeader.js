fbc321c6b852b5d6d34571dc94b9449dfdf329e8
Anton Ladan <ladananton450@gmail.com>
2023-03-18 19:15:25 +0100

    fix: header

--- frontend/src/components/DashboardHeader/DashboardHeader.js
+++ frontend/src/components/DashboardHeader/DashboardHeader.js
@@ -4,9 +4,14 @@ import './DashboardHeader.scss';
 
 const DashboardHeader = () => {
     return (
-        <div className='container'>
-        
-        </div>
-    )}
+        <header className=" bg-blue-900  flex items-center justify-between px-4 py-3 border-b-2 border-color: rgb(71,86,180) rounded-b-lg">
+            <div className="flex items-center ">
+                <img src='' alt="ikona logo" className="h-8 w-8 mr-2" />
+                <span className="text-2xl font-bold">SPS</span>
+            </div>
+            <span className="text-sm font-medium">username</span>
+        </header>
+    )
+}
 
 export default DashboardHeader
