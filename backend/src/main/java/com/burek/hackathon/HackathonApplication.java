07df75267752de5efde09a55be45ec991459f64d
AlexIchenskiy <aichenskiy@gmail.com>
2023-03-18 14:50:59 +0100

    feat: add user CRUD operations

--- backend/src/main/java/com/burek/hackathon/HackathonApplication.java
+++ backend/src/main/java/com/burek/hackathon/HackathonApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.burek.hackathon.*"})
 @ComponentScan(basePackages = "com.burek.*")
 @EnableJpaRepositories(basePackages = "com.burek.hackathon")
 public class HackathonApplication {
