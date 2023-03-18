cf2feedd577c7f4d5cfc10627fd89d7a6ed72cb9
Iva Kvesic <ivakvesic16@gmail.com>
2023-03-18 17:42:48 +0100

    chore: add tailwind

--- frontend/src/pages/LogIn/Login.js
+++ frontend/src/pages/LogIn/Login.js
@@ -2,33 +2,48 @@ import React from 'react';
 import backgroundImage from '../../assets/images/pozadina.jpg';
 
 const Login = () => {
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <div className='loginWindow' style={{ textAlign: 'center' }}>
-          <h1>Log In</h1>
-          <form>
-            <label htmlFor='email'>Email:</label>
-            <input type='email' id='email' name='email' required />
-            <br />
-            <label htmlFor='password'>Password:</label>
-            <input type='password' id='password' name='password' required />
-            <br />
-            <button type='submit'>Login</button>
-          </form>
-        </div>
+  return (
+    <div
+      className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="loginWindow text-center">
+        <h1 className="text-3xl font-bold mb-6">Log In</h1>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-lg font-medium mb-2">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="bg-gray-200 rounded-lg px-4 py-2 w-full"
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-lg font-medium mb-2">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="bg-gray-200 rounded-lg px-4 py-2 w-full"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white rounded-lg px-4 py-2 font-medium"
+          >
+            Login
+          </button>
+        </form>
       </div>
-    );
-  };
-  
-  export default Login;
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/SignUp/Signup.js
+++ frontend/src/pages/SignUp/Signup.js
@@ -2,36 +2,22 @@ import React from 'react';
 import backgroundImage from '../../assets/images/pozadina.jpg';
 
 const Signup = () => {
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <div className='signupWindow' style={{ textAlign: 'center' }}>
-          <h1>Sign Up</h1>
-          <form>
-            <label htmlFor='username'>Username:</label>
-            <input type='username' id='username' name='username' required />
-            <br />
-            <label htmlFor='email'>Email:</label>
-            <input type='email' id='email' name='email' required />
-            <br />
-            <label htmlFor='password'>Password:</label>
-            <input type='password' id='password' name='password' required />
-            <br />
-            <button type='submit'>Sign Up</button>
-          </form>
-        </div>
+  return (
+    <div className="h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="signupWindow text-center">
+        <h1 className="text-4xl font-bold mb-8">Sign Up</h1>
+        <form className="mb-4">
+          <label htmlFor='username' className="block font-medium text-gray-700 mb-2">Username:</label>
+          <input type='username' id='username' name='username' required className="w-full p-2 border border-gray-400 rounded-md mb-4" />
+          <label htmlFor='email' className="block font-medium text-gray-700 mb-2">Email:</label>
+          <input type='email' id='email' name='email' required className="w-full p-2 border border-gray-400 rounded-md mb-4" />
+          <label htmlFor='password' className="block font-medium text-gray-700 mb-2">Password:</label>
+          <input type='password' id='password' name='password' required className="w-full p-2 border border-gray-400 rounded-md mb-4" />
+          <button type='submit' className="bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline">Sign Up</button>
+        </form>
       </div>
-    );
-  };
-  
-  export default Signup;
+    </div>
+  );
+};
+
+export default Signup;
