d362c33071f3e59e965b8e304a2e7b2f59951c7e
Anton Ladan <ladananton450@gmail.com>
2023-03-18 14:45:29 +0100

    feat: added sections

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import DashboardSections from '../../components/DashboardSections/DashboardSections';
 import DashboardHeader from '../../components/DashboardHeader/DashboardHeader';
-
+import ThreatIntelligence from '../../components/ThreatIntelligence/ThreatIntelligence';
+import RealTimeMonitoring from '../../components/RealTimeMonitoring/RealTimeMonitoring';
+import Alerts from '../../components/Alerts/Alerts';
+import ReportingAndAnalysis from '../../components/ReportingAndAnalysis/ReportingAndAnalysis';
+import SystemParameters from '../../components/SystemParameters/SystemParameters';
+import Help from '../../components/Help/Help';
 
 const Dashboard = () => {
+    let selection = <ThreatIntelligence />
+
     return (
         <>
             <DashboardSections />
-                <div style={{display: 'inline-block'}}>
-                    <DashboardHeader />
-                </div>
+            <div style={{display: 'inline-block'}}>
+                <DashboardHeader />
+                {selection}
+            </div>
         </>
     )
 }
