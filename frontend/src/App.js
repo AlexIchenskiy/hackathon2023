b009790a5cabbdf8984828010621ee44d1bb0b79
Iva Kvesic <ivakvesic16@gmail.com>
2023-03-18 16:28:34 +0100

    feat: add basic navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Home from "./pages/Home/Home";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from "./pages/LogIn/Login";
+import Signup from "./pages/SignUp/Signup";
+
+
 function App() {
-  return <></>;
+  
+ return(
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/signup" element={<Signup />} />
+</Routes>
+ )
 }
 
 export default App;
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import Login from '../../components/LogIn.js';
+import {Link} from 'react-router-dom';
 import backgroundImage from '../../assets/images/Human-factor_cybersecurity.webp';
 
 const Home = () => {
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <div className='homeTitle' style={{ textAlign: 'center' }}>
-          <h1>Spider Cyber Security</h1>
-          <div>
-            <button>Login</button>
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <div className='homeTitle' style={{ textAlign: 'center' }}>
+        <h1>Spider Cyber Security</h1>
+        <div>
+          
+          <Link to='/login'>
+            <button>Log In</button>
+          </Link>
+          <Link to='/signup'>
             <button>Sign Up</button>
-          </div>
+          </Link>
         </div>
-        {/* Your content here */}
-
-        <>
-        <Login />
-        </>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Home;
