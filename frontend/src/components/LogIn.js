5d944b508be497b025efa156f5abd32e5c6e78c0
Iva Kvesic <ivakvesic16@gmail.com>
2023-03-18 13:21:21 +0100

    feat: add home page components and basic webpage

---
+++ frontend/src/components/LogIn.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import backgroundImage from '../assets/images/Human-factor_cybersecurity.webp';
+
+const Login = () => {
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <div className='loginWindow' style={{ textAlign: 'center' }}>
+          <h1>Log In</h1>
+          <form>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' name='email' required />
+            <br />
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password' name='password' required />
+            <br />
+            <button type='submit'>Login</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Login;
