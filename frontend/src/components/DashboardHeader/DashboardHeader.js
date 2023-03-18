346485b97a08d38ed7ce9691bd6b5aa3b7c97eea
Anton Ladan <ladananton450@gmail.com>
2023-03-18 20:04:24 +0100

    feat: add logic to switch subpages in dashboard

--- frontend/src/components/DashboardHeader/DashboardHeader.js
+++ frontend/src/components/DashboardHeader/DashboardHeader.js
@@ -6,7 +6,6 @@ import logo from "../../assets/images/ikona_spider.png";
 
 const DashboardHeader = () => {
     return (
-
         <header className=" bg-blue-900  flex items-center justify-between px-4 py-3 border-b-2 border-color: rgb(71,86,180) rounded-b-lg">
             <div className="flex items-center ">
                 <img src={logo} alt="ikona logo" className="h-8 w-8 mr-2" />
@@ -16,6 +15,5 @@ const DashboardHeader = () => {
         </header>
     )
 }
-
 
 export default DashboardHeader
--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
 import DashboardHeader from '../../components/DashboardHeader/DashboardHeader';
 import DashboardNav from '../../components/DashboardNav/DashboardNav';
+import ThreatIntelligence from '../../components/ThreatIntelligence/ThreatIntelligence';
+import RealTimeMonitoring from '../../components/RealTimeMonitoring/RealTimeMonitoring';
+import Alerts from '../../components/Alerts/Alerts';
+import ReportingAndAnalysis from '../../components/ReportingAndAnalysis/ReportingAndAnalysis';
+import SystemParameters from '../../components/SystemParameters/SystemParameters';
+import Help from '../../components/Help/Help';
 
 const Dashboard = () => {
     const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(<ThreatIntelligence />);
+
+    const listItems = [
+        <ThreatIntelligence />,
+        <RealTimeMonitoring />,
+        <Alerts />,
+        <ReportingAndAnalysis />,
+        <SystemParameters />,
+        <Help />,
+    ];
 
     const handleItemSelected = (index) => {
         setSelectedIndex(index);
+        setSelectedItem(listItems[index]);
     }
 
     return (
-        <>
+        <div className='w-full h-full'>
             <DashboardHeader />
             <DashboardNav onItemSelected={handleItemSelected} />
-            {selectedIndex}
-        </>
+            <div className='inline-block'>
+                {selectedItem}
+            </div>
+        </div>
     )
 }
 
