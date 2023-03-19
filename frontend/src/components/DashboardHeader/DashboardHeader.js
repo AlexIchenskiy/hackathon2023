58ededd79d4aff2ec9f34cc37991d35cb5af073d
Anton Ladan <ladananton450@gmail.com>
2023-03-19 04:13:17 +0100

    feat: add severity filter for threat intelligence

--- frontend/src/components/DashboardHeader/DashboardHeader.js
+++ frontend/src/components/DashboardHeader/DashboardHeader.js
@@ -1,35 +1,44 @@
 import React from "react";
-
 import { useState } from "react";
 
-import "./DashboardHeader.scss";
+import FilterBySeverity from "../ThreatIntelligence/components/FilterBySeverity/FilterBySeverity";
 
+import "./DashboardHeader.scss";
 import logo from "../../assets/images/ikona_spider.png";
 
 const DashboardHeader = () => {
+  const [showWindow, setShowWindow] = useState(false);
 
-      return (
-        <header className='bg-light-violet flex items-center justify-between px-4 py-3 '>
-          <div className="flex items-center">
-            <img src={logo} alt="ikona logo" className="h-12 w-12 mr-2" />
-            <span className="text-5xl font-bold font-family: ui-monospace">SCS</span>
-          </div>
-          <span className={`border-red border-solid border-2 px-3 py-3 rounded-3xl text-lg font-medium cursor-pointer transition-all duration-300 ${showWindow ? 'bg-red text-white' : ''}`}
-            onClick = {handleWindowToggle}
-          >
-            username
-          </span>
+  function handleWindowToggle() {
+    setShowWindow(!showWindow);
+  }
 
-          {showWindow && (
-            <div className="p-6 border-dashed divide-x-4 border-4 border-red rounded-b-2xl bg-white z-10 absolute right-0 mt-56 mr-4 right-5">
-            <div className='py-1 '> User info </div>
-            <div className='py-1'> User settings</div>
-            <div className='py-1'> Logout</div>
-            </div>
-   
-  )}
-</header>
-        
-    )}
+  return (
+    <header className="bg-light-violet flex items-center justify-between px-4 py-3 ">
+      <div className="flex items-center">
+        <img src={logo} alt="ikona logo" className="h-12 w-12 mr-2" />
+        <span className="text-5xl font-bold font-family: ui-monospace">
+          SCS
+        </span>
+      </div>
+      <span
+        className={`border-red border-solid border-2 px-3 py-3 rounded-3xl text-lg font-medium cursor-pointer transition-all duration-300 ${
+          showWindow ? "bg-red text-white" : ""
+        }`}
+        onClick={handleWindowToggle}
+      >
+        username
+      </span>
 
-export default DashboardHeader
+      {showWindow && (
+        <div className="p-6 border-dashed divide-x-4 border-4 border-red rounded-b-2xl bg-white z-10 absolute right-0 mt-56 mr-4 right-5">
+          <div className="py-1 "> User info </div>
+          <div className="py-1"> User settings</div>
+          <div className="py-1"> Logout</div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default DashboardHeader;
